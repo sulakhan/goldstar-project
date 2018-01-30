dd591391a17c8166fd6658e9b4b2ebb8058de220
sulakhan <sulakhan.singh@logicielsolutions.co.in>
2018-01-30 17:23:13 +0530

    adding div elemnt

---
+++ header.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Header Page</title>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width">
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="css/jquery.fancybox.min.css">
+		<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+		<link rel="stylesheet" type="text/css" href="css/loader.css">
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+	</head>
+<body class="main">
+<!--Header Start-->
+<header>
+	<!--Navbar Start-->
+	<nav class="navbar navbar-default navbar-bg navbar-fixed-top">
+		<div class="container contain-struct">
+			<!-- Brand and toggle get grouped for better mobile display -->
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a href="index.php">
+					<div>
+						<img src="images/goldstar-logo.png" alt="golden-star">
+					</div>
+				</a>
+			</div>
+			<!-- Collect the nav links, forms, and other content for toggling -->
+			<div class="collapse navbar-collapse nav-collapse pull-right" id="bs-example-navbar-collapse-1">
+				<ul class="nav navbar-nav">
+					<li>
+						<a href="index.php" class="active">Home 
+							<span class="sr-only">(current)</span>
+						</a>
+					</li>
+					<li>
+						<a href="blog.php">Blog</a>
+					</li>
+					<li>
+						<a href="gallery.php">Gallery</a>
+					</li>
+					<li>
+						<a href="about.php">About</a>
+					</li>
+					<li>
+						<a href="products.php">Products</a>
+					</li>
+					<li>
+						<a href="contact.php">Contact Us</a>
+					</li>
+					<li>
+						<a href="follow.php">Follow Us</a>
+					</li>
+					<li>	
+						<a href="#">
+							<i class="fa fa-search" aria-hidden="true"></i>
+						</a>
+					</li>
+				</ul>
+			</div><!-- /.Navbar-Collapse -->
+		</div>
+	</nav><!--Navbar End-->
+	<div>1</div>
+</header>
+<!--Header End-->
+<!--Page Loader Start-->
+<div id="loader-wrapper">
+	<div id="loader"></div>
+	<div class="loader-section section-left"></div>
+	<div class="loader-section section-right"></div>
+</div>
+<!--Page Loader End-->
